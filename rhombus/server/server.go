11eb16e86e993dc65bd8eb87c944cad28923c83c
EnoRage <enormousrage@gmail.com>
2019-02-16 16:27:44 -0700

    add rhombus service

--- rhombus/server/server.go
+++ rhombus/server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	_ "../docs"
 	"../ethereum"
+	"../responses"
+	"github.com/gin-gonic/contrib/cors"
 	"github.com/gin-gonic/gin"
-	"log"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"net/http"
 )
 
@@ -11,25 +15,30 @@ type ServerInstance struct {
 	Engine *gin.Engine
 }
 
-func RunServer()  {
+func RunServer() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	s := ServerInstance{
 		Engine: r,
 	}
-	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-
+	r.Use(cors.Default())
 	r.GET("/rate/:currency", s.GetCurrencyToUSD)
-
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":8080")
 
 }
 
-
-
+// @Summary Return exchange rate of specific currency group
+// @Description  Return exchange rate of specific currency group
+// @Accept  text/plain
+// @Produce  application/json
+// @Param   currency     path    string     true        "currency"
+// @Success 200 {array} responses.ExchangeResponse
+// @Router /rate/{currency} [get]
+//GetCurrencyToUSD Return exchange rate of specific currency group
 func (s *ServerInstance) GetCurrencyToUSD(c *gin.Context) {
-	log.Println(c.Param("currency"))
-	c.JSON(http.StatusOK, gin.H{
-		"rate":  (ethereum.GetRate(c.Param("currency"))),
-	})
+	r := responses.ExchangeResponse{}
+	r.Rate = ethereum.GetRate(c.Param("currency"))
+	c.JSON(http.StatusOK, r)
 }
