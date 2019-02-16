de58edcd9e4731f40d772126f8f110eb562f3b17
artemvorobev <artall64@gmail.com>
2019-02-16 14:30:55 -0700

    work on celer - new keys

--- frontend/src/js/celer/index.js
+++ frontend/src/js/celer/index.js
@@ -2,8 +2,8 @@ window.account1 = "149960814b05d5560bba5000f6c9852c250611bd";
 window.account2 = "f6cdf7cdc1d804765c481d03ce0e545d073219d9";
 
 window.eth2celarhost = {
-  "49ca2717e582e4037fe4c07eacf6ca3933a8f62d": "https://ethergram.tk/c0",
-  "90c8c14e90353730b605cd37231f3021bbfdc741": "https://ethergram.tk/c1"
+  "149960814b05d5560bba5000f6c9852c250611bd": "https://ethergram.tk/c0",
+  "f6cdf7cdc1d804765c481d03ce0e545d073219d9": "https://ethergram.tk/c1"
 };
 
 function getClient(ethAddress) {
