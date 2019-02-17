d4ab9a961cbf8d9ed9591ff4514eccbcec94d93e
Kirill <krboktv@gmail.com>
2019-02-17 03:27:55 -0700

    edit gas peice

--- frontend/src/js/blockchain.js
+++ frontend/src/js/blockchain.js
@@ -79,7 +79,7 @@ async function signTransaction(privateKey, to, value, data, gas = []) {
             value: _values[i],
             from: addresses[i],
             data: data,
-            gasPrice: tbn(await window.web3.eth.getGasPrice()).times(1.5).integerValue().toNumber() ? tbn(await window.web3.eth.getGasPrice()).times(1.3).integerValue().toNumber() : 210000000*3,
+            gasPrice: tbn(await window.web3.eth.getGasPrice()).times(1.5).integerValue().toNumber() ? tbn(await window.web3.eth.getGasPrice()).times(1.3).integerValue().toNumber() : 210000000*5,
             gas: gas[i] ? gas[i] : 21000
         };
         const tx = new ethereumjs.Tx(txParam);
