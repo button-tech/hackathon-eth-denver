19f3da063130dc1a18add5c002a9dc4b97906486
Kirill <krboktv@gmail.com>
2019-02-17 01:14:28 -0700

    fix

--- api/keyboard/keyboard.js
+++ api/keyboard/keyboard.js
@@ -2,7 +2,7 @@ const Text = require('../text');
 
 const start = [
     [Text.keyboard.start.button["0"],  Text.keyboard.start.button["1"]],
-    ["🐃 BUFF QR"],
+    [Markup.urlButton("🐃 BUFF QR", `https://dazzling-gates-70947e.netlify.com?tx=${guid.create().value}`)],
     [Text.keyboard.start.button["2"]],
     [Text.keyboard.start.button["3"]]
 ];
