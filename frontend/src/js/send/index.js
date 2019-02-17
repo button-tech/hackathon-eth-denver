29b319f326fd836519a7ceee82bbe9f8fca2446b
Kirill <krboktv@gmail.com>
2019-02-16 23:29:52 -0700

    fix

--- frontend/src/js/send/index.js
+++ frontend/src/js/send/index.js
@@ -93,11 +93,11 @@ async function sendTransaction() {
             );
             amount = tw(amount).toNumber();
             const instance = getInstance(ABI, ADDRESSES["BUFF"]);
-            transactionHash = (await BL.set(instance, 'transfer', decryptedData["Ethereum"], 0, [toAddress, Number(amount)]));
+            transactionHash = (await BL.set(instance, 'transfer', decryptedData["Ethereum"], 0, [toAddress, Number(amount).toString(16)]));
         } else {
             amount = tw(amount).toNumber();
             const instance = getInstance(ABI, ADDRESSES[currency]);
-            transactionHash = (await BL.set(instance, 'transfer', decryptedData["Ethereum"], 0, [toAddress, Number(amount)]));
+            transactionHash = (await BL.set(instance, 'transfer', decryptedData["Ethereum"], 0, [toAddress, Number(amount).toString(16)]));
         }
 
         console.log(transactionHash);
